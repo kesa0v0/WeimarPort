2ecc3c50ef6c1e27b87fd1df9d456bed7f93ad47
kesa <kesa0v0@gmail.com>
2025-09-19 00:19:43 +0900

    Add UIOtherPartyStatus prefab and related scripts
    - Created a new prefab for UIOtherPartyStatus and added it to the MainScene.
    - Implemented UIOtherPartyStatusModel, UIOtherPartyStatusPresenter, and UIOtherPartyStatusView scripts to manage the UI logic.
    - Added necessary metadata files for the new scripts and prefab.
    - Set up UI elements in UIOtherPartyStatusView for displaying party information.

---
+++ Assets/Scripts/UI/UIOtherPartyStatusView.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIOtherPartyStatusView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI partyNameText;
+    [SerializeField] private TextMeshProUGUI partyStatusText;
+    [SerializeField] private TextMeshProUGUI partyAgendaText;
+    [SerializeField] private ScrollView partyUnitsListView;
+
+    
+}
