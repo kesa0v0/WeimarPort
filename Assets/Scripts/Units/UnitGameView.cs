d86142eb727d4d5c925e6424a9f0851559814eb1
kesa <kesa0v0@gmail.com>
2025-09-24 22:34:17 +0900

    Update UnitGameView to adjust rotation and ensure proper scaling when attaching to city

--- Assets/Scripts/Units/UnitGameView.cs
+++ Assets/Scripts/Units/UnitGameView.cs
@@ -30,7 +30,7 @@ public class UnitGameView : BaseUnitView, IPointerClickHandler
         // 로컬 좌표계를 사용하여 도시 기준으로 붙이고, 기본 위치는 (0,0,0)
         transform.SetParent(anchor, false);
         transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.Euler(-90, 0, 0); // 원하는 각도
+        transform.localRotation = Quaternion.Euler(90, 0, 180); // 원하는 각도
         transform.localScale = ScaleFix.FixScale(transform.parent, new Vector3(0.1f, 0.1f, 0.1f));
     }
 
